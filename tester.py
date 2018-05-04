3922f070b2da191d49902952edc45cb13fd600eb
Ethan <ethanrobison@u.northwestern.edu>
2018-05-03 22:33:38 -0500

    format

--- tester.py
+++ tester.py
@@ -1,64 +1,67 @@
-import subprocess, os, glob
+import subprocess
+import os
+import glob
+
 
 class TestRunner(object):
-	"""Builds a student's project; then runs unit tests on it."""
-	def __init__(self, config, assignment_dll, test_dll):
-		super(TestRunner, self).__init__()
-		
-		self._mstest = '"' + config.mstest_path + '"' # this is kind of yikes, but, sigh
-		self._msbuild = '"' + config.msbuild_path + '"'
+    """Builds a student's project; then runs unit tests on it.
+    Grading is handled elsewhere, though - nota bene."""
 
-		self._assignment_dll = assignment_dll
-		self._test_dll = test_dll
+    def __init__(self, config, assignment_dll, test_dll):
+        super(TestRunner, self).__init__()
 
-		self._test_string = self._mstest + ' "/testcontainer:"' + self._test_dll + ' "/detail:errormessage" >| out.txt' # sorry; this sucks
+        self._mstest = '"' + config.mstest_path + \
+            '"'  # this is kind of yikes, but, sigh
+        self._msbuild = '"' + config.msbuild_path + '"'
 
+        self._assignment_dll = assignment_dll
+        self._test_dll = test_dll
 
-	def RunTests(self, submission):
-		print('Building solution for submission: \t' + submission.submission_id)
-		self.BuildStudentDLL(submission.directory)
-		self.PrepareStudentDLL(submission.directory, self._assignment_dll)
+        self._test_string = self._mstest + ' "/testcontainer:"' + self._test_dll + \
+            ' "/detail:errormessage" >| out.txt'  # sorry; this sucks
 
-		os.chdir('./sandbox')
-		if os.path.isfile('out.txt'): os.remove('out.txt')
+    def RunTests(self, submission):
+        print('Building solution for submission: \t' + submission.submission_id)
+        self.BuildStudentDLL(submission.directory)
+        self.PrepareStudentDLL(submission.directory, self._assignment_dll)
 
-		print('Running unit tests for submission: \t' + submission.submission_id)
-		subprocess.Popen(self._test_string, stdout=subprocess.PIPE, shell=True)
+        os.chdir('./sandbox')
+        if os.path.isfile('out.txt'):
+            os.remove('out.txt')
 
-		os.chdir('./..')
+        print('Running unit tests for submission: \t' + submission.submission_id)
+        subprocess.Popen(self._test_string, stdout=subprocess.PIPE, shell=True)
 
-		print()
-		return True
+        os.chdir('./..')
 
+        print()
+        return True
 
-	def BuildStudentDLL(self, search_directory):
-		results = glob.glob(search_directory + '/**/*.sln', recursive=True)
-		if results: target = results[0] # here's hoping that they have only one .sln
-		if not target: assert False, 'Failed to find a solution in directory: ' + search_directory
+    def BuildStudentDLL(self, search_directory):
+        results = glob.glob(search_directory + '/**/*.sln', recursive=True)
+        if results:
+            target = results[0]  # here's hoping that they have only one .sln
+        if not target:
+            assert False, 'Failed to find a solution in directory: ' + search_directory
 
-		buildpath = self._msbuild + ' "' + target + '"'
-		output = subprocess.Popen(buildpath, stdout=subprocess.PIPE, shell=True).stdout.read()
-		
-		# TODO this is a bit of a mess
-		if '0 Errores' not in str(output): assert False, 'Either it borked or you need to handle me better. Directory: ' + search_directory
+        buildpath = self._msbuild + ' "' + target + '"'
+        output = subprocess.Popen(
+            buildpath, stdout=subprocess.PIPE, shell=True).stdout.read()
 
+        # TODO this is a bit of a mess
+        if '0 Errores' not in str(output):
+            assert False, 'Either it borked or you need to handle me better. Directory: ' + search_directory
 
+    def PrepareStudentDLL(self, search_directory, assignment, sandbox_dir='sandbox/'):
+        goal_path = os.path.join(sandbox_dir, assignment)
+        if os.path.isfile(goal_path):
+            os.remove(goal_path)
 
-	def PrepareStudentDLL(self, search_directory, assignment, sandbox_dir='sandbox/'):
-		goal_path = os.path.join(sandbox_dir, assignment)
-		if os.path.isfile(goal_path): os.remove(goal_path)
+        path = self.FindAssignmentDLLPath(search_directory, assignment)
+        os.rename(path, goal_path)
 
-		path = self.FindAssignmentDLLPath(search_directory, assignment)
-		os.rename(path, goal_path)
-
-
-	def FindAssignmentDLLPath(self, directory, assignment_name):
-		results = glob.glob(directory + '/**/Assignment1.dll', recursive=True)
-		if results: return results[0]
-		assert False, 'Failed to find a DLL and this error is not well-handled.'
-
-# "/mnt/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/MSTest.exe" "/testcontainer:QueueTests.dll" "/detail:errormessage" >| out.txt
-
-if __name__ == '__main__':
-	tester = TestRunner('','Assignment1.dll','QueueTests.dll')
-	tester.RunTests('')
+    def FindAssignmentDLLPath(self, directory, assignment_name):
+        results = glob.glob(directory + '/**/Assignment1.dll', recursive=True)
+        if results:
+            return results[0]
+        assert False, 'Failed to find a DLL and this error is not well-handled.'
